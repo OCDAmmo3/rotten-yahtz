8f706f85eab6f39b61de9b998e0c33851006a663
Jon Struve <ocdammo3@gmail.com>
2023-11-01 16:06:54 -0500

    Added D5 and D21, not sure where to start with rarity or Low Roller yet

--- diceScenes/dicePossibilities/dice_selectable_options.gd
+++ diceScenes/dicePossibilities/dice_selectable_options.gd
@@ -5,18 +5,35 @@ extends Node2D
 		"name": "D6",
 		"node": DiceOptions.dice_options.D6,
 		"sprite": preload("res://diceScenes/assets/diceFaceImages/d6/dice-6.png"),
-		"tooltip": "Standard 6 sided dice with values 1-6"
+		"tooltip": "Standard 6 sided dice with values 1-6",
+		"rarity": "C"
 	},
 	{
 		"name": "D4",
 		"node": DiceOptions.dice_options.D4,
 		"sprite": preload("res://diceScenes/assets/diceFaceImages/d4/4-sided-4.png"),
-		"tooltip": "Standard 4 sided dice with values 1-4"
+		"tooltip": "Standard 4 sided dice with values 1-4",
+		"rarity": "C"
 	},
 	{
 		"name": "D2",
 		"node": DiceOptions.dice_options.D2,
 		"sprite": preload("res://diceScenes/assets/diceFaceImages/d2/coin_dice_5.png"),
-		"tooltip": "Standard 2 sided coin with values 1 and 2"
+		"tooltip": "Standard 2 sided coin with values 1 and 2",
+		"rarity": "C"
+	},
+	{
+		"name": "D5",
+		"node": DiceOptions.dice_options.D5,
+		"sprite": preload("res://diceScenes/assets/diceFaceImages/d6/dice-5.png"),
+		"tooltip": "Standard 5 sided dice with values 1-5",
+		"rarity": "C"
+	},
+	{
+		"name": "D21",
+		"node": DiceOptions.dice_options.D21,
+		"sprite": preload("res://diceScenes/assets/diceFaceImages/d6/dice-6.png"),
+		"tooltip": "21 sided dice with 6 sixes, 5 fives, 4 fours, 3 threes, 2 twos and 1 one",
+		"rarity": "R"
 	}
 ]
